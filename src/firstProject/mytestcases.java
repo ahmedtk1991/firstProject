e8e9cdd03375bcd7bce90e0a369fe0da0b2c9f5a
ahmedtk1991 <l3younek@gmail.com>
2024-10-22 06:01:32 +0200

    update

---
+++ src/firstProject/mytestcases.java
@@ -0,0 +1,43 @@
+package firstProject;
+
+import java.lang.classfile.instruction.NewMultiArrayInstruction;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class mytestcases {
+	
+	WebDriver driver = new ChromeDriver();
+	
+	
+@BeforeTest
+	public void Setup ()  {
+	
+	driver.get("https://www.google.com");
+}
+	
+	
+	
+	
+	@Test
+	
+	public void SerchAboutMySelf ()  {}
+	
+	
+	
+	
+	
+	@AfterTest  
+	public void AfterMyTest ()  {}
+	
+	
+
+	
+	
+	
+	
+
+}
