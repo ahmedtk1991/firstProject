6d4475801f60aac400bf52f0597b4c753716d2a9
ahmedtk1991 <l3younek@gmail.com>
2024-10-22 06:15:16 +0200

    update

--- src/firstProject/mytestcases.java
+++ src/firstProject/mytestcases.java
@@ -4,12 +4,13 @@ import java.lang.classfile.instruction.NewMultiArrayInstruction;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class mytestcases {
-	
+	WebDriver driver2 = new EdgeDriver();
 	WebDriver driver = new ChromeDriver();
 	
 	
@@ -19,7 +20,6 @@ public class mytestcases {
 	driver.get("https://www.google.com");
 }
 
- //ahmed
 
 	@Test
 	
